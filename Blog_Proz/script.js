c3b71d359d6b90deec63946afe30b67162ef9729
Priscilha <priscilha@gmail.com>
2023-07-21 20:53:54 -0300

    Blog Proz Conexão HTML CSS

---
+++ Blog_Proz/script.js
@@ -0,0 +1,13 @@
+//const teste = document.getElementById('teste');
+
+//console.log(teste)
+
+//const porTag = document.getElementsByTagName('li')
+
+//console.log(porTag)
+
+
+const autor = document.getElementsByClassName('post-autor')
+
+console.log(autor[0])
+
